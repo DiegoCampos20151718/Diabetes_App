f62a9d38abb9444d76cdc53f2a98a0fa99c605a6
Axel Ramos <anotherman16@gmail.com>
2024-11-11 02:59:36 -0600

    Register form working

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bcrypt
@@ -8,9 +8,12 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Cambia esto por una clave secreta más segura
 
 # Conectar a MongoDB
-client = MongoClient("mongodb+srv://alexcamdel254:KQVI1NIGDBkQpgme@cluster0.ryqs3.mongodb.net/")
+#client = MongoClient("mongodb+srv://alexcamdel254:KQVI1NIGDBkQpgme@cluster0.ryqs3.mongodb.net/")
+
+client = MongoClient("mongodb+srv://pythonapp:pythonapp123@cluster0.r0bbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 db = client['Diabetes']
-users_collection = db['Users']
+users_collection = db['users']
 health_records_collection = db['health_records']
 
 # Ruta para mostrar el formulario de login
@@ -23,7 +26,7 @@ def login_page():
 def health_form():
     return render_template('health_form.html')
 
-@app.route('/Home')
+@app.route('/home')
 def Home():
     if 'username' in session and 'role' in session:
         return render_template('index.html', username=session['username'], role=session['role'])
@@ -39,9 +42,42 @@ def login():
     if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
         session['username'] = user['username']
         session['role'] = user.get('role', 'User')  # Valor por defecto 'User' si no existe
-        return redirect(url_for('Home'))
+        return jsonify({"status": "success", "message": "Inicio de sesión exitoso."}), 200
     else:
-        return "Usuario o contraseña incorrecta."
+        return jsonify({"status": "error", "message": "Usuario o contraseña incorrecta."}), 401
+    
+# Ruta para mostrar el formulario de register
+@app.route('/register_form')
+def register_form():
+    return render_template('register.html')
+
+# Ruta para manejar el registro
+@app.route('/register', methods=['POST'])
+def register():
+    registeredUser = {
+        "name": request.form['name'],
+        "last_name": request.form['last_name'],
+        "username": request.form['rusername'],
+        "password": request.form['rpassword'],
+        "role": "User"
+    }
+    
+    # Verifica si el usuario ya existe
+    existing_user = users_collection.find_one({"username": registeredUser["username"]})
+    if existing_user:
+        return jsonify({"status": "error", "message": "¡Woops! El nombre de usuario ya existe, elige otro."}), 409
+    
+    # Encripta la contraseña
+    password = request.form['rpassword'].encode('utf-8')
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    # Completa el registro del usuario con la contraseña encriptada
+    registeredUser["password"] = hashed_password
+    registeredUser["role"] = "User"
+    
+    # Inserta el nuevo usuario en la colección
+    users_collection.insert_one(registeredUser)
+    return jsonify({"status": "success", "message": "¡Registro exitoso! Inicia sesión."}), 201
 
 # Ruta para recibir y procesar los datos de salud
 @app.route('/submit_health_data', methods=['POST'])
